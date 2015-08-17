8c2221a26104a61d8db53f2bb03a1c6e5be604fd
Vincent St-Amour <stamourv@racket-lang.org>
2015-08-17 17:08:11 -0500

    Move tests from unstable.

---
+++ unix-socket-test/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define collection 'multi)
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.10")))
+(define build-deps '("rackunit-lib"))
+(define pkg-authors '(ryanc))
