d795f7e455e815b058feda19dd3605f9d9ac90cd
Ryan Culpepper <ryanc@racket-lang.org>
2016-02-16 13:58:28 -0500

    make connect non-blocking

--- unix-socket-lib/racket/unix-socket.rkt
+++ unix-socket-lib/racket/unix-socket.rkt
@@ -13,7 +13,6 @@ macosx (64):
 |#
 
 ;; TODO:
-;; - nonblocking connect
 ;; - listener (bind/listen/accept)
 
 (require racket/contract
@@ -46,9 +45,23 @@ macosx (64):
 (define unix-socket-available?
   (and platform #t))
 
-
 (define AF-UNIX 1)
 (define SOCK-STREAM 1)
+(define SOCK_NONBLOCK #o00004000)
+
+;; linux: asm-generic/{errno-base,errno}.h; macosx: sys/errno.h
+(define EINTR           4)
+(define EAGAIN          (case platform [(linux) 11]  [(macosx) 35]))
+(define EWOULDBLOCK     EAGAIN)
+(define EINPROGRESS     (case platform [(linux) 115] [(macosx) 36]))
+
+;; linux: asm-generic/fcntl.h; macosx: sys/fcntl.h
+(define F_SETFL         4)
+(define O_NONBLOCK      (case platform [(linux) #o4000] [(macosx) 4]))
+
+;; linux: asm-generic/socket.h; macosx: sys/socket.h
+(define SOL_SOCKET      (case platform [(linux) 1] [(macosx) #xFFFF]))
+(define SO_ERROR        (case platform [(linux) 4] [(macosx) #x1007]))
 
 (define UNIX-PATH-MAX
   (case platform
@@ -91,8 +104,28 @@ macosx (64):
   (_fun #:save-errno 'posix
         _int --> _int))
 
+(define-libc fcntl
+  (_fun #:save-errno 'posix
+        _int _int _int --> _int))
+
+(define-libc getsockopt
+  (_fun #:save-errno 'posix
+        _int _int _int (value : (_ptr io _int) = 0) (len : (_ptr io _uint32) = (ctype-sizeof _int))
+        --> (result : _int)
+        --> (cond [(zero? result)
+                   value]
+                  [else
+                   (define errno (saved-errno))
+                   (error 'getsockopt "error\n  errno: ~a~a" errno (errno-error-line errno))])))
+
 (define-libc scheme_make_fd_output_port
   (_fun _int _racket _bool _bool _bool --> _racket))
+
+(define-libc scheme_fd_to_semaphore
+  (_fun _intptr _int _bool --> _racket))
+
+(define MZFD_CREATE_READ 1)
+(define MZFD_CREATE_WRITE 2)
 
 (define strerror-name
   (case platform
@@ -164,6 +197,11 @@ macosx (64):
     (let ([errno (saved-errno)])
       (error who "failed to create socket\n  errno: ~a~a"
              errno (errno-error-line errno))))
+  (unless (zero? (fcntl socket-fd F_SETFL O_NONBLOCK))
+    (close socket-fd)
+    (let ([errno (saved-errno)])
+      (error who "failed to put socket in non-blocking mode\n  errno: ~a~a"
+             errno (errno-error-line errno))))
   (values socket-fd (register-custodian-shutdown socket-fd close)))
 
 ;; close/unregister : Cust-Reg -> Void
@@ -171,22 +209,52 @@ macosx (64):
   (close fd)
   (unregister-custodian-shutdown fd reg))
 
+;; make-socket-ports : Symbol FD Cust-Reg -> (values Input-Port Output-Port)
+(define (make-socket-ports who socket-fd reg)
+  (with-handlers ([(lambda (e) #t)
+                   (lambda (exn)
+                     (close/unregister socket-fd reg)
+                     (raise exn))])
+    (begin0 (scheme_make_fd_output_port socket-fd 'unix-socket #f #f #t)
+      ;; closing the ports closes socket-fd, so custodian no longer needs to manage directly
+      (unregister-custodian-shutdown socket-fd reg))))
+
 ;; unix-socket-connect : Path/String -> (values Input-Port Output-Port)
 (define (unix-socket-connect path)
   (check-platform 'unix-socket-connect)
   (define-values (sockaddr addrlen) (do-make-sockaddr 'unix-socket-connect path))
-  (call-as-atomic
-   (lambda ()
-     (define-values (socket-fd reg) (do-make-socket 'unix-socket-connect))
-     (unless (zero? (connect socket-fd sockaddr addrlen))
-       (close/unregister socket-fd reg)
-       (let ([errno (saved-errno)])
-         (error 'unix-socket-path->bytes "failed to connect socket\n  path: ~e\n  errno: ~a~a"
-                path errno (errno-error-line errno))))
-     (with-handlers ([(lambda (e) #t)
-                      (lambda (exn)
-                        (close/unregister socket-fd reg)
-                        (raise exn))])
-       (begin0 (scheme_make_fd_output_port socket-fd 'unix-socket #f #f #t)
-         ;; Closing the ports closes socket-fd
-         (unregister-custodian-shutdown socket-fd reg))))))
+  (define connect-k
+    ;; Non-blocking connect may succeed immediately or require waiting to see.
+    ;; - If succeeds immediately, make ports in same atomic block
+    ;; - If wait, must exit atomic mode to sync
+    ;; So we return a procedure to be applied in non-atomic mode that does 
+    ;; whatever needs doing.
+    (call-as-atomic
+     (lambda ()
+       (define-values (socket-fd reg) (do-make-socket 'unix-socket-connect))
+       (define r (connect socket-fd sockaddr addrlen))
+       (define errno (saved-errno))
+       (cond [(= r 0) ;; connected
+              (define-values (in out) (make-socket-ports 'unix-socket-connect socket-fd reg))
+              (lambda () (values in out))]
+             [(= errno EINPROGRESS) ;; wait and see
+              (define sema (scheme_fd_to_semaphore socket-fd MZFD_CREATE_WRITE))
+              (lambda () ;; called in non-atomic mode!
+                (sync sema)
+                ;; FIXME: check custodian hasn't been shut down?
+                (call-as-atomic
+                 (lambda ()
+                   (define errno (getsockopt socket-fd SOL_SOCKET SO_ERROR))
+                   (cond [(= errno 0)
+                          (make-socket-ports 'unix-socket-connect socket-fd reg)]
+                         [else
+                          (close/unregister socket-fd reg)
+                          (error 'unix-socket-connect
+                                 "failed to connect socket (non-blocking)\n  path: ~e\n  errno: ~a~a"
+                                 path errno (errno-error-line errno))]))))]
+             [else
+              (close/unregister socket-fd reg)
+              (let ([errno (saved-errno)])
+                (error 'unix-socket-connect "failed to connect socket\n  path: ~e\n  errno:~a~a"
+                       path errno (errno-error-line errno)))]))))
+  (connect-k))
